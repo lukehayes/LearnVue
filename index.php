f66b2eae1efb0c8f5546618cf49119d08ada7a5d
lukehayes <lukepurplehayes@gmail.com>
2020-09-22 13:52:17 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vue</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> 
+</head>
+<body>
+
+<div id="app">
+<input type="text" v-model="n">
+{{n}}
+    <ul>
+        <todo-item v-for="todo in todos" v-bind:todo="todo">{{todo}}</todo-item>
+    </ul>
+</div>
+
+<script charset="utf-8" src="main.js"></script>
+</body>
+</html>
