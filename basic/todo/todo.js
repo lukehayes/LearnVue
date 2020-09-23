de8cc700805accecea7a2799e1286a65eb7f2e29
lukehayes <lukepurplehayes@gmail.com>
2020-09-23 16:40:35 +0100

    Added addButton helper function

--- basic/todo/todo.js
+++ basic/todo/todo.js
@@ -6,14 +6,24 @@ let app = document.getElementById('app');
 let notes = document.getElementById('notes');
 let noteInput = document.getElementById('note-input');
 
+function addButton(title) {
+    let elem = document.createElement('button');
+    elem.innerText = title;
+    return elem;
+}
+
 noteInput.addEventListener('keydown', function(e) {
     if(e.keyCode == 13)
     {
         let note = e.target.value;
         let elem = document.createElement("DIV");
+        elem.className = "note";
         elem.innerText = note;
 
+        let btn = addButton('Remove');
+
         notes.appendChild(elem);
+        notes.appendChild(btn);
     }
 });
 
