3c3cd2078ad78bca1ca2c1a8b77e18711433d594
lukehayes <lukepurplehayes@gmail.com>
2021-02-17 15:42:15 +0000

    Main.js now holds Vue instance

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 
+Vue.config.performance = true;
+Vue.config.warnHandler = function(msg, vm, trace)
+{
+    console.debug("ERROR! See, warnHandler callback function.");
+}
 
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{todo.item}} - <span>{{todo.completed}}</span></li>'
-})
+console.debug(Vue.config);
 
 let app = new Vue({
-    'el' : '#app',
-    'data' : {
-        col: "#FF11aF;",
-        n : 100,
-        todos: [
-            { item: "Learn Vue.js", completed: false},
-            { item: "Build a Startup", completed: false},
-            { item: "Read The Pragmatic Programmer", completed: false},
-        ]
-    },
-
-    methods : {
-        clicker: function()
-        {
-            console.log("Clicked.");
-        }
+    el: '#app',
+    data: {
+        text: 'Application Instance'
     }
 });
+
+
+console.debug(app);
