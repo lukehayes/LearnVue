ae0d2d263657d2846a3e15fb2a26a07c8f9fa98f
lukehayes <lukepurplehayes@gmail.com>
2021-02-17 17:36:24 +0000

    Added markup for debugging purposes

--- index.php
+++ index.php
@@ -3,21 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <title>App</title>
-    <link rel="stylesheet" href="basic/todo/style.css" title="" type="">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+
 </head>
 <body>
 
+<h3>Users</h3>
+
 <div id="app">
-    
-    <div id="note-form">
-        <input type="text" id="note-input" placeholder="Add a new note...">
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th v-if="show">Website</th>
+            </tr>
+        </thead>
 
-    <div id="notes"></div>
-
+        <tbody>
+            <tr v-for="user in users">
+  <button class="button" v-on:click="show = !show">
+    Toggle
+  </button>
+                <td>{{ user.name }}</td>
+                <td>{{ user.username }}</td>
+                <transition name="fade">
+                    <td v-if="show">{{ user.website }}</td>
+                </transition>
+                
+            </tr>
+        </tbody>
+    </table>
+    <ul>
+    </ul>
 </div>
-
-<script charset="utf-8" src="basic/todo/todo.js" type="module"></script>
-<!--<script charset="utf-8" src="main.js"></script> -->
+    
+<!-- <script charset="utf-8" src="basic/todo/todo.js" type="module"></script> -->
+<script charset="utf-8" src="main.js"></script>
 </body>
 </html>
