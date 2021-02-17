dc1f04ced2fcf794ce73cebde5a93e613bf589d7
lukehayes <lukepurplehayes@gmail.com>
2021-02-17 17:36:50 +0000

    main.js now using a simply Vue instance

--- main.js
+++ main.js
@@ -5,14 +5,34 @@ Vue.config.warnHandler = function(msg, vm, trace)
     console.debug("ERROR! See, warnHandler callback function.");
 }
 
-console.debug(Vue.config);
-
 let app = new Vue({
     el: '#app',
     data: {
-        text: 'Application Instance'
+        text: 'Application Instance',
+        users: null,
+        show: true
+    },
+
+    methods: {
+        request: function()
+        {
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => this.users = json);
+            //let request = new XMLHttpRequest();
+            //request.open('https://jsonplaceholder.typicode.com/posts/1');
+            //request.send();
+
+            //console.debug(request);
+
+        }
+    },
+
+    mounted: function()
+    {
+        console.log(this.request());
     }
+
 });
 
-
-console.debug(app);
