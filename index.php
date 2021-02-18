c55a1b439469a24556a182af973885df9f7f93d0
lukehayes <lukepurplehayes@gmail.com>
2021-02-18 17:30:14 +0000

    Renamed vue-todo.js to todo-list.js

--- index.php
+++ index.php
@@ -8,8 +8,12 @@
 </head>
 <body>
 
+<h3>Vue Todo App</h3>
+
 <div id="app">
+    <p v-for="todo in todos">{{ todo.item }}</p>
 </div>
 
+<script src="components/todo/todo-list.js"></script>
 </body>
 </html>
