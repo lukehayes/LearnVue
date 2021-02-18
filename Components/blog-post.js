4e978a5ecac1ddbbfeef27682bcab85ad4ab6cb8
lukehayes <lukepurplehayes@gmail.com>
2021-02-18 12:33:54 +0000

    Blog-post component data now returns an empty object

--- Components/blog-post.js
+++ Components/blog-post.js
@@ -1,13 +1,18 @@
 
 Vue.component('blog-post', {
 
-
     props: ['title'],
 
     data: function()
     {
+        return {
+        }
     },
 
     template: '<h3>{{ title }}</h3>'
 
 })
+
+let vm = new Vue({
+    el : "#blog-posts"
+});
