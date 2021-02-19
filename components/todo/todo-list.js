bb448e31c061205c4cc700c9c4786dd7797d40b4
lukehayes <lukepurplehayes@gmail.com>
2021-02-19 13:23:38 +0000

    Moved todo-item component into todo-list.js

--- components/todo/todo-list.js
+++ components/todo/todo-list.js
@@ -1,12 +1,18 @@
+Vue.component('todo-item', {
+    props: {
+        item: Object
+    },
+    template: '<p> {{item.id}} - {{item.text}} </p>'
+})
+
 let app = new Vue({
     'el' : '#app',
     'data' : {
         col: "#FF11aF;",
-        n : 100,
         todos: [
-            { item: "Learn Vue.js", completed: false},
-            { item: "Build a Startup", completed: false},
-            { item: "Read The Pragmatic Programmer", completed: false},
+            { id:1, item: "Learn Vue.js", completed: false},
+            { id:2, item: "Build a Startup", completed: false},
+            { id:3, item: "Read The Pragmatic Programmer", completed: false},
         ]
     },
 
