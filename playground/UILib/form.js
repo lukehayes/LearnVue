34406c7c383e8d7f4a5e53d7845bda7df3a3cc69
lukehayes <lukepurplehayes@gmail.com>
2021-02-23 15:48:55 +0000

    Added docblock comments to form.js

--- playground/UILib/form.js
+++ playground/UILib/form.js
@@ -1,5 +1,17 @@
 /**
- * Basic FORM Markup Generation
+ * UILibForm
+ *
+ * HTML form creation library.
+ */
+
+/**
+ * Create an HTML Form.
+ *
+ * @param {string}  method    The type of input field('text', 'email' etc).
+ * @param {string}  action    The input fields attribute value.
+ * @param {Element} parent    The parent element it should be appended to.
+ *
+ * @return {Element} The newly created HTML form.
  */
 function UIForm(method, action, parent)
 {
@@ -10,6 +22,15 @@ function UIForm(method, action, parent)
     return element;
 }
 
+/**
+ * Create an input field for a form.
+ *
+ * @param {string}  The type of input field('text', 'email' etc).
+ * @param {string}  The input fields attribute value.
+ * @param {Element} The parent element it should be appended to.
+ *
+ * @return {Element} The newly created input field
+ */
 function UIFormInput(type, value, parent)
 {
     let element = document.createElement("INPUT");
@@ -17,4 +38,18 @@ function UIFormInput(type, value, parent)
     element.value = value || "";
     parent.appendChild(element);
     return element;
+}
+
+/**
+ * Attact an event to a form element.
+ *
+ * @param {string}  formInput    Element to attach the event to.
+ * @param {string}  event        The name of the event.
+ * @param {Element} callback     The callback to run when the event occurs.
+ *
+ * @return {Element} The newly created input field
+ */
+function UIFormEvent(formInput, event, callback)
+{
+    formInput.addEventListener(event, callback);
 }
