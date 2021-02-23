a5ad31152a48ca0f7063286a371650ca771dc1bc
lukehayes <lukepurplehayes@gmail.com>
2021-02-23 17:49:52 +0000

    Removed creation functions into there own files

--- playground/main.js
+++ playground/main.js
@@ -1,24 +1,8 @@
-'use strict';
 
-var VectorBase = function()
-{
-    this.x = 1;
-    this.y = 1;
-}
+let root = document.getElementById("app");
 
-VectorBase.prototype.get = function()
-{
-    let res = this.x + " " + this.y;
-    return res;
-}
+let form = UIForm('GET', '/', root)
 
-let vb = new VectorBase();
+UIFormInput('text', 'Username', form);
 
-function Point()
-{
-    VectorBase.call(this);
-}
 
-Point.prototype = new VectorBase();
-
-let p = new Point();
